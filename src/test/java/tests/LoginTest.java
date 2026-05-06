9b8e580622bf367df047e82241373dd3ea5286f7
Bonolo Jeanet <bonolojeanet@gmail.com>
2026-05-06 19:14:36 +0200

    Fix TestNG setup and add test class

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test
+    public void openGoogle() {
+        System.out.println("Test is running");
+    }
+}
